cca8751023930414110dcaf96064599f0db656bc
brunorsnts <santos_2002@outlook.com.br>
2026-04-01 09:53:49 -0300

    Adiciona configuracoes de CORS

--- src/main/java/br/com/bsantos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/bsantos/screenmatch/ScreenmatchApplication.java
@@ -1,30 +1,13 @@
 package br.com.bsantos.screenmatch;
 
-import br.com.bsantos.screenmatch.principal.Principal;
-import br.com.bsantos.screenmatch.repositories.SerieRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Locale;
-
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
+public class ScreenmatchApplication {
 
-    private final SerieRepository serieRepository;
-
-    public ScreenmatchApplication(SerieRepository serieRepository) {
-        this.serieRepository = serieRepository;
-    }
 
     public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
-
-    @Override
-    public void run(String... args) throws Exception {
-        Locale.setDefault(Locale.US);
-        Principal principal = new Principal(serieRepository);
-        principal.exibeMenu();
-    }
 }
